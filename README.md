a57ed471bd4c9fa61bd2b872a6d4395b8255b714
Will Beaufoy <will@willbeaufoy.net>
2017-01-14 23:46:04 +0000

    Add some structural changes
    - Remove reference to old location API
    - Remove main function

--- README.md
+++ README.md
@@ -1,85 +1,20 @@
-Ultimate traceroute / Geolocated traceroute with whois
-====================
+TraceroutePlus
+=============
 
-Adds GeoIP lookup and whois information to traceroute. Running ultimate traceroute to a host will output the IP address, location and ownership information for all hops in the journey.
+Adds various interesting pieces of information to a standard traceroute call, including AS number, information from a whois call for each hop, and server location calculated using an IP location service. Intended purely for satisfying curiosity. 
+
+Modified from VisualTraceroute by Kyle Flanagan.
 
 Usage
-======
+----------
 
-./vistraceroute.py google.com
+Uses Python 3 and works on Linux. Ensure the tracerouteplus.py file is set to be executable.
 
-Other to look at
-====================
+```bash
+./tracerouteplus.py google.com
+```
 
-https://github.com/ayeowch/traceroute
+IP location service
+------------------------------
 
-Modified from VisualTraceroute by Kyle Flanagan. His original README below
-==========================================================================
-
-http://kyleflan.wordpress.com/2012/12/13/a-visual-traceroute-program/
-
-Visual Traceroute - vistraceroute v 0.1
-(C) 2012 Kyle Flanagan
-This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-------------------------------------------------------------------------------
-Description
-------------------------------------------------------------------------------
-Visual Traceroute is a program that uses either the posix or Windows 
-traceroute/rt program to find IP addresses of gateways along a traceroute 
-route. The program then uses information from www.ipinfodb.com to get the 
-locations of these gateways. Finally, the program constructs a URL using 
-Google's static map API to display these locations and the path the route 
-takes.
-
-Under Linux, the program uses traceroute
-Under Windows, the program uses tracert
-You must have these programs installed to use vistraceroute
-
-Misc:
-The map markers are labeled with the order of each IP address returned from
-traceroute (save for the gateways that didn't return any information; these
-are ignored). However, after 9, the markers are labeled with simply a dot (.)
-because of marker label limitations in the static Google map.
-
-------------------------------------------------------------------------------
-Usage
-------------------------------------------------------------------------------
-Note: You may have to make the files vistraceroute.py and pyipinfodb.py 
-executable under Linux (chmod +x vistraceroute.py, chmod +x pyipinfodb.py).
-
-vistraceroute.py ip_address(or hostname)
-
-alternatively
-
-python vistraceroute.py ip_address(or hostname)
-
-The latter usage is necessary to run the program under Windows. Make sure that
-you're either in the Python directory or that the location of python.exe is in
-your system path.
-
-Example usage:
-python vistraceroute.py www.google.com
-
-Once the program is finished, it will display a URL. Copy and paste this URL
-in a web browser to view the map.
-
-------------------------------------------------------------------------------
-Contact
-------------------------------------------------------------------------------
-If you find bugs (of which I'm sure there are many) or have any suggestions,
-email me.
-Kyle Flanagan
-kyleflanagan@gmail.com
-
+Currently the script uses [ipinfo.io's](http://ipinfo.io/) API (free for 1000 requests per day), but could easily be configured to use a different service.
--- tracerouteplus.py
+++ tracerouteplus.py
@@ -1,150 +1,117 @@
 #!/usr/bin/env python3
 
-# (C) Kyle Flanagan 2012
-# vistraceroute
-#
-# This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 import pyipinfoio
 import sys, re, subprocess
+from urllib.parse import urlparse
 
-def main():
+if len(sys.argv) < 2:
+  print("Usage: trp <ip_address / hostname>")
+  sys.exit()
 
-    if len(sys.argv) < 2:
-        print("Usage: trp <ip_address / hostname>")
-        return
+address = sys.argv[1]
+print('\n***** Running TraceroutePlus on {} *****\n'.format(address))
 
-    IP = sys.argv[1]
+# Full response headers of destination
+print('***** Full response headers of destination *****\n')
+subprocess.call(['curl', '-I', address])
 
-    # Full whois of destination
-    # print('***** Full whois of destination *****\n')
-    # subprocess.call(['whois', '-H', IP])
+# Start traceroute
+print("***** TraceroutePlus hops *****\n")
 
-    # Full response headers of destination
-    print('\n***** Full response headers of destination *****\n')
-    subprocess.call(['curl', '-I', IP])
+traceroute = 'traceroute'
+args = [address]
+args.insert(0, '-A') # Add Autonomous System (AS) path lookups
 
-    # Start traceroute
-    print("\n***** TraceroutePlus to destination *****\n")
+args.insert(0, traceroute)
 
-    traceroute = 'traceroute'
-    args = [IP]
-    # args.insert(0, '-m30') # Limit maximum num of hops to 30
-    # args.insert(0, '-w1') # Limit maximum wait time per hop to 1 sec
-    args.insert(0, '-A') # Add Autonomous System (AS) path lookups
+tr_process = subprocess.Popen(args,
+  shell=False,
+  stdout=subprocess.PIPE)
 
-    args.insert(0, traceroute)
-    # args now looks like: traceroute, [flag,] IP
-    # start traceroute
+for line in tr_process.stdout:
+  line = line.decode("utf-8")
 
-    print("Starting traceroute...\n")
-    # tr_process = subprocess.check_output(args)
-    tr_process = subprocess.Popen(args,
-            shell=False,
-            stdout=subprocess.PIPE)
+  # Print whole traceroute line for testing
+  # print(line)
 
-    # Each line from the process will be 'dealt with' by the for loop as it appears.
-    for line in tr_process.stdout:
-        line = line.decode("utf-8")
+  # Skip the first line as it contains the traceroute 'intro'
+  if re.search('^traceroute', line):
+    continue
 
-        # Print whole traceroute line for testing
-        # print(line)
+  split_line = line.split('  ')
+  output = {}
 
-        # Skip the first line as it contains the traceroute 'intro'
-        if re.search('^traceroute', line):
-            continue
+  if '* * *' in line:
+    output['hop'] = split_line[0].strip()
+    print('Hop: ' + output['hop'] + ' ***\n')
+    continue
 
-        split_line = line.split('  ')
-        output = {}
+  # Split the line and extract the hostname, IP address and AS number
+  output['hop'] = split_line[0].strip()
+  output['hostname'] = re.findall('^(.+)\s', split_line[1])[0]
+  output['ip'] = re.findall('\((.+)\)', split_line[1])[0]
+  output['asn'] = re.findall('\[(.+)\]', split_line[1])[0]
+  output['rtt1'] = split_line[2]
+  output['rtt2'] = split_line[3]
+  output['rtt3'] = split_line[4]
 
-        if '* * *' in line:
-            output['hop'] = split_line[0].strip()
-            print('Hop: ' + output['hop'] + ' ***\n')
-            continue
+  # start whois
+  process = subprocess.Popen(['whois', '-H', output['ip']], shell=False, stdout=subprocess.PIPE)
 
-        # Split the line and extract the hostname, IP address and AS number
-        output['hop'] = split_line[0].strip()
-        output['hostname'] = re.findall('^(.+)\s', split_line[1])[0]
-        output['ip'] = re.findall('\((.+)\)', split_line[1])[0]
-        output['asn'] = re.findall('\[(.+)\]', split_line[1])[0]
-        output['rtt1'] = split_line[2]
-        output['rtt2'] = split_line[3]
-        output['rtt3'] = split_line[4]
+  if process.wait() != 0:
+    print("Whois error. Exiting.")
+    sys.exit()
 
-        # start whois
-        process = subprocess.Popen(['whois', '-H', output['ip']], shell=False, stdout=subprocess.PIPE)
+  # read Registrant Organisation from whois
+  whois_output = process.communicate()[0]
+  whois_output = whois_output.decode("utf-8")
 
-        if process.wait() != 0:
-            print("Whois error. Exiting.")
-            return
-        
-        # read Registrant Organisation from whois
-        whois_output = process.communicate()[0]
-        whois_output = whois_output.decode("utf-8")
+  # print raw whois output for testing
+  # print(whois_output)
 
-        # print raw whois output for testing
-        # print(whois_output)
+  if re.findall("descr:.*", whois_output):
+    output['descr'] = re.findall("descr:.*", whois_output)[0].replace('descr:', '').strip()
+  if re.findall("person:.*", whois_output):
+    output['person'] = re.findall("person:.*", whois_output)[0].replace('person:', '').strip()
+  if re.findall("address:.*", whois_output):
+    output['address'] = re.findall("address:.*", whois_output)[0].replace('address:', '').strip()
+  if re.findall("OrgName:.*", whois_output):
+    output['OrgName'] = re.findall("OrgName:.*", whois_output)[0].replace('OrgName:', '').strip()
+  if re.findall("org-name:.*", whois_output):
+    output['org-name'] = re.findall("org-name:.*", whois_output)[0]
+  if re.findall("country:.*", whois_output):
+    output['country'] = re.findall("country:.*", whois_output)[0].replace('country:', '').strip()
+  if re.findall("Registrant Name:.*", whois_output):
+    output['Registrant Name'] = re.findall("Registrant Name:.*", whois_output)[0]
+  if re.findall("Registrant Organisation:.*", whois_output):
+    output['Registrant Organisation'] = re.findall("Registrant Organisation:.*", whois_output)[0]
+  if re.findall("Registrant Street:.*", whois_output):
+    output['Registrant Street'] = re.findall("Registrant Street:.*", whois_output)[0]
+  if re.findall("Registrant City:.*", whois_output):
+    output['Registrant City'] = re.findall("Registrant City:.*", whois_output)[0]
 
-        if re.findall("descr:.*", whois_output):
-            output['descr'] = re.findall("descr:.*", whois_output)[0].replace('descr:', '').strip()
-        if re.findall("person:.*", whois_output):
-            output['person'] = re.findall("person:.*", whois_output)[0].replace('person:', '').strip()
-        if re.findall("address:.*", whois_output):
-            output['address'] = re.findall("address:.*", whois_output)[0].replace('address:', '').strip()
-        if re.findall("OrgName:.*", whois_output):
-            output['OrgName'] = re.findall("OrgName:.*", whois_output)[0].replace('OrgName:', '').strip()
-        if re.findall("org-name:.*", whois_output):
-            output['org-name'] = re.findall("org-name:.*", whois_output)[0]
-        if re.findall("country:.*", whois_output):
-            output['country'] = re.findall("country:.*", whois_output)[0].replace('country:', '').strip()
-        if re.findall("Registrant Name:.*", whois_output):
-            output['Registrant Name'] = re.findall("Registrant Name:.*", whois_output)[0]
-        if re.findall("Registrant Organisation:.*", whois_output):
-            output['Registrant Organisation'] = re.findall("Registrant Organisation:.*", whois_output)[0]
-        if re.findall("Registrant Street:.*", whois_output):
-            output['Registrant Street'] = re.findall("Registrant Street:.*", whois_output)[0]
-        if re.findall("Registrant City:.*", whois_output):
-            output['Registrant City'] = re.findall("Registrant City:.*", whois_output)[0]
+  ipinfoio_data = pyipinfoio.get_ip_location(output['ip'])
+  # Assume if city exists then region will too, at least as a key
+  if 'city' in ipinfoio_data and ipinfoio_data['city'] != '':
+    output['location'] = '{}, {}'.format(ipinfoio_data['city'], ipinfoio_data['region'])
 
-        # Removed this functionality Jan 2016 as website stopped working
-        # ipi = IPInfo(API_KEY, output['ip'], city=True) # get city, state
-        
-        ipinfoio_data = pyipinfoio.get_ip_location(output['ip'])
-        # Assume if city exists then region will too, at least as a key
-        if 'city' in ipinfoio_data and ipinfoio_data['city'] != '':
-            output['location'] = '{}, {}'.format(ipinfoio_data['city'], ipinfoio_data['region'])
-
-        print('Hop: ' + output['hop'])
-        print('Hostname: ' + output['hostname'])
-        print('IP: ' + output['ip'])
-        print('AS Number: ' + output['asn'])
-        if 'descr' in output:
-            print('Descr: ' + output['descr'])
-        if 'OrgName' in output:
-            print('OrgName: ' + output['OrgName'])
-        if 'person' in output:
-            print('Contact Person: ' + output['person'])
-        if 'address' in output:
-            print('Contact Address: ' + output['address'])
-        if 'Registrant Name' in output:
-            print('Registrant Name: ' + output['Registrant Name'])
-        if 'location' in output:
-            print('Server location: ' + output['location'])
-        if 'country' in output:
-            print('Country: ' + output['country'])
-        print('RTT: ' + output['rtt1'] + ', ' + output['rtt2'] + ', ' + output['rtt3'])
-        print('')
-
-if __name__ == "__main__":
-    main()
+  print('Hop: ' + output['hop'])
+  print('Hostname: ' + output['hostname'])
+  print('IP: ' + output['ip'])
+  print('AS Number: ' + output['asn'])
+  if 'descr' in output:
+    print('Descr: ' + output['descr'])
+  if 'OrgName' in output:
+    print('OrgName: ' + output['OrgName'])
+  if 'person' in output:
+    print('Contact Person: ' + output['person'])
+  if 'address' in output:
+    print('Contact Address: ' + output['address'])
+  if 'Registrant Name' in output:
+    print('Registrant Name: ' + output['Registrant Name'])
+  if 'location' in output:
+    print('Server location: ' + output['location'])
+  if 'country' in output:
+    print('Country: ' + output['country'])
+  print('RTT: ' + output['rtt1'] + ', ' + output['rtt2'] + ', ' + output['rtt3'])
+  print('')
